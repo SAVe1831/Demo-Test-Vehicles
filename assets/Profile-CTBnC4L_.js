a35040dcdbf8fa25c0e43b83d79e929aba5d8411
SAVe1831 <save1831537@gmail.com>
2024-06-09 09:55:04 +0300

    Updates

--- assets/Profile-CTBnC4L_.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,o as t,a as e}from"./index-DUaFQb1t.js";const c={},_={class:"m-5"},n=e("h1",null,"Profile",-1),a=e("p",null,"Figma говорит, что здесь пусто",-1),r=[n,a];function i(l,d){return t(),s("div",_,r)}const h=o(c,[["render",i]]);export{h as default};
--- assets/Setting-CadU4cnq.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as s,o,a as e}from"./index-DUaFQb1t.js";const c={},n={class:"m-5"},_=e("h1",null,"Setting",-1),a=e("p",null,"Figma говорит, что здесь пусто",-1),r=[_,a];function i(d,l){return o(),s("div",n,r)}const h=t(c,[["render",i]]);export{h as default};
